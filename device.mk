d6a11b726aa432d049ddbb12fdf86dea8460b081
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-07-31 08:42:08 -0300

    platina: add powerhint from common Signed-off-by: allansouzadk <allan.pereira.araujo02@gmail.com> Signed-off-by: allansouzadk <allansouzadk@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -325,6 +325,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-sdm660-libperfmgr
 
 PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
 
 # Protobuf
