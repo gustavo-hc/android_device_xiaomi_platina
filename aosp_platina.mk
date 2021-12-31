4a80daf5c99981553704762b46a9f8dbad7012fd
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-12-31 00:49:38 -0300

    platina: use aosp recovery

--- aosp_platina.mk
+++ aosp_platina.mk
@@ -27,6 +27,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_USES_AOSP_RECOVERY := true
 
 # Inherit from platina device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
