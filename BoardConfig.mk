1ef7fd9f2f02b37d46829638da5c84124bd3e43a
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-29 13:16:13 -0300

    platina:  Disable custom inode count calculation for SSI partitions

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,7 +127,7 @@ BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := $(ALL_PARTITIONS)
 
 # Partitions - reserved size
 $(foreach p, $(call to-upper, $(SSI_PARTITIONS)), \
-    $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := 6144))
+    $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := -1))
 $(foreach p, $(call to-upper, $(TREBLE_PARTITIONS)), \
     $(eval BOARD_$(p)IMAGE_EXTFS_INODE_COUNT := 5120))
 
